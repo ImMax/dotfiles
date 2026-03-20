0cf1c36f18312907280e0616d7140b779fea6884
Max Khmelevskii <mk.void.mail@gmail.com>
2026-03-20 16:02:27 +0100

    fix init.sh

--- init.sh
+++ init.sh
@@ -9,6 +9,6 @@ NVIM_CONF=".config/nvim"
 NVIM_INIT="$NVIM_CONF/init.lua"
 
 mkdir -p "$HOME/$NVIM_CONF"
-ln -s "nvim/$NVIM_INIT" "$HOME/$NVIM_INIT"
+ln -s "$PWD/nvim/$NVIM_INIT" "$HOME/$NVIM_INIT"
 ls -l "$HOME/$NVIM_INIT"
 
