751f2f62dd7ae03652b316b8bbb146036211de9d
Max Khmelevskii <mk.void.mail@gmail.com>
2026-03-20 15:34:50 +0100

    add nvim

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+set -e
+# do tmux
+
+# do neovim
+
+NVIM_CONF=".config/nvim"
+NVIM_INIT="$NVIM_CONF/init.lua"
+
+mkdir -p "$HOME/$NVIM_CONF"
+ln -s "nvim/$NVIM_INIT" "$HOME/$NVIM_INIT"
+ls -l "$HOME/$NVIM_INIT"
+
