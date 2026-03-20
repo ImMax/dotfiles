f21005a88c07f5e85ab3b944e7761ec3e741d0d2
Max Khmelevskii <mk.void.mail@gmail.com>
2026-03-20 16:22:27 +0100

    remove remote plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -463,24 +463,6 @@ require('lazy').setup({
       },
     },
   },
-  {
-    'OscarCreator/rsync.nvim',
-    build = 'make',
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = function()
-      require('rsync').setup()
-    end,
-  },
-  {
-    'amitds1997/remote-nvim.nvim',
-    version = '*', -- Pin to GitHub releases
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- For standard functions
-      'MunifTanjim/nui.nvim', -- To build the plugin UI
-      'nvim-telescope/telescope.nvim', -- For picking b/w different remote methods
-    },
-    config = true,
-  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
