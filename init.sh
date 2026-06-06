0aa6bf624906e24e3d7305feb74b4a999eb5ad7a
Max Khmelevskii <mk.void.mail@gmail.com>
2026-06-06 23:05:36 +0200

    add nvim tmux git

--- init.sh
+++ init.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/bash
 
 set -e
+
+DOTFILES_DIR="$HOME/dotfiles"
+BACKUP_DIR="$HOME/.dotfiles-backup"
+NVIM_CONF="$HOME/.config/nvim"
+TMUX_CONF="$HOME/.tmux.conf"
+GIT_CONFIG="$HOME/.gitconfig"
+
+# do backup
+echo "==> Backing up existing dotfiles to $BACKUP_DIR"
+
+mkdir -p "$BACKUP_DIR"
+cp -r "$NVIM_CONF" "$BACKUP_DIR"
+cp "$TMUX_CONF" "$BACKUP_DIR"
+
 # do tmux
+echo "==> Create .tmux.conf symlink"
+ln -s "$DOTFILES_DIR/tmux/tmux-main.conf" "$TMUX_CONF" 
 
 # do neovim
+echo "==> Create nvim symlink"
+ln -s "$DOTFILES_DIR/nvim/nvim-main" "$HOME/$NVIM_INIT"
 
-NVIM_CONF=".config/nvim"
-NVIM_INIT="$NVIM_CONF/init.lua"
-
-mkdir -p "$HOME/$NVIM_CONF"
-ln -s "$PWD/nvim/$NVIM_INIT" "$HOME/$NVIM_INIT"
-ls -l "$HOME/$NVIM_INIT"
-
+# do git
+echo "==> Create .gitconfig symlink"
+ln -s "$DOTFILES_DIR/git/gitconfig" "$GIT_CONFIG"
