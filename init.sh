64e98be6ff171c7ef0c542fc8ecfb0c135719e75
Max Khmelevskii <mk.void.mail@gmail.com>
2026-06-18 22:24:12 +0200

    add git hooks

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ BACKUP_DIR="$HOME/.dotfiles-backup"
 NVIM_CONF="$HOME/.config/nvim"
 TMUX_CONF="$HOME/.tmux.conf"
 GIT_CONFIG="$HOME/.gitconfig"
+GIT_HOOKS_GLOB="$HOME/.githooks"
 TMUX_TPM="$HOME/.tmux/plugins/tpm"
 
 # do backup
@@ -14,6 +15,7 @@ echo "==> Backing up existing dotfiles to $BACKUP_DIR"
 mkdir -p "$BACKUP_DIR"
 [ -e "$NVIM_CONF" ] && cp -r "$NVIM_CONF" "$BACKUP_DIR"
 [ -e "$TMUX_CONF" ] && cp "$TMUX_CONF" "$BACKUP_DIR"
+[ -e "$GIT_HOOKS_GLOB" ] && cp "$GIT_HOOKS_GLOB" "$BACKUP_DIR"
 
 # do tmux
 echo "==> Create .tmux.conf symlink"
@@ -27,3 +29,5 @@ ln -sf "$DOTFILES_DIR/nvim/nvim-main" "$HOME/$NVIM_INIT"
 # do git
 echo "==> Create .gitconfig symlink"
 ln -sf "$DOTFILES_DIR/git/gitconfig" "$GIT_CONFIG"
+echo "==> Create .githooks symlink"
+ln -sf "$DOTFILES_DIR/git/githooks" "$GIT_HOOKS_GLOB"
